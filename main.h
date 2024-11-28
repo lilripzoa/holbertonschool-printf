4e258ae676cfefd02701c42b396b6cfdfff4987c
karen <kiziasuma@gmail.com>
2024-11-28 15:20:01 +0100

    update

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "main.h"
+#include <stdarg.h>
+#include <unistd.h> /* pour ecrire*/
+
+int _printf(const char *format, ...);
+int print_char(char c); /* Prototype de print_char*/
+int print_string(char *str); /* Prototype de print_string*/
+
+#endif /* MAIN_H */
